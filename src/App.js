e70a6dd0628150b7903959b26efe5362590334b6
Purnabhishek Sripathi <76512306+abhisripathi@users.noreply.github.com>
2023-09-18 20:18:54 -0700

    Update App.js
    update-1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
 
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import HTMLLoader from './HTMLLoader';
+
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  return (
+    <Router>
+      <div>
+
+        <Route path="/itc505/homework/2" exact>
+          <HTMLLoader url={process.env.PUBLIC_URL + '/2.html'} />
+        </Route>
+  
+      </div>
+    </Router>
+  );
 }
 
 export default App;
